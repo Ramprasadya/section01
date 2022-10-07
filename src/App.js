46164100713db7dce7bff85ff3a9e481fab0dc4a
Ramprasadya <yadavramprasad563@gmail.com>
2022-10-07 11:07:30 +0530

    Added carousel and images

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Carausal from "./Components/Carausal/Carausal";
+
+function App() {
+  return (
+
+
+    <>
+      <h1>Welcome Interns</h1>
+   <h2>This project is under work</h2>
+    <Carausal/>
+    
+
+    </>
+
+    
+
+  );
+}
+
+export default App;
